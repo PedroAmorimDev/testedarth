cb13f609bc45a80452c4936a1bf9c28c9e6db4f0
Pedro Henrique <138811347+PedroAmorimDev@users.noreply.github.com>
2024-02-18 20:29:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburguer = document.querySelector(".hamburguer")
+const navMenu = document.querySelector(".navmenu")
+hamburguer.addEventListener("click", ()=>{
+   hamburguer.classList.toggle('active')
+   navMenu.classList.toggle('active')
+})
